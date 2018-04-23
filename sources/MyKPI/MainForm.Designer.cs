8662c23d1d22c878a725ab0885ef5d15befa1b33
DESKTOP-NC5SGMD\Tran Huu Hoa <tranhuuhoa1481986@gmail.com>
2018-04-23 22:35:02 +0700

    test commit

--- sources/MyKPI/MainForm.Designer.cs
+++ sources/MyKPI/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyKPI
 {
-    partial class Form2
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
+
