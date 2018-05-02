0e07a0984a4276621e0a0fed53afc1bbacac05ea
DESKTOP-NC5SGMD\Tran Huu Hoa <tranhuuhoa1481986@gmail.com>
2018-05-02 21:09:00 +0700

    add job kpi entities

--- sources/MyKPI/Entities/JobKpiEntity.cs
+++ sources/MyKPI/Entities/JobKpiEntity.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using MyKPI.Common;
+using MyKPI.Entities.Assessment;
 
 namespace MyKPI.Entities
 {
     class JobKpiEntity
     {
+        EmployeeEntity Employee { get; set; }
+        JobRankValue RoleInAssessment { get; set; }
+        DateTime CreatedDate { get; set; }
+        IProjectContribution ProjectContribution1 { get; set; }
+        IProjectContribution ProjectContribution2 { get; set; }
+        IProjectContribution ProjectContribution3 { get; set; }
+        IProfessionalContribution ProfessionalContribution { get; set; }
+        ISkillAssessment SkillAssessment { get; set; }
+
     }
 }
